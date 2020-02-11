c1f084e11ff83fdd1d00c0b36482ce595e59a017
Patrick595 <patrickbuong@gmail.com>
2020-02-11 15:26:35 +0300

    created a pitch class

--- app/models.py
+++ app/models.py
@@ -34,4 +34,13 @@ class User(UserMixin,db.model)
         self.pass_secure = generate_password_hash(password)
 
     def verify_password(self, password):
-        
+        return check_password_hash(password)
+
+        def __repr__(self)
+            return f'{self.username}'
+
+
+
+class pitch(db.model):
+    '''
+    '''
