e04ff684866ca5a187963726e49608d05e7867cf
Patrick595 <patrickbuong@gmail.com>
2020-02-11 15:06:22 +0300

    defined a function that generates the password of users

--- app/models.py
+++ app/models.py
@@ -8,3 +8,22 @@ from werzeug.security import generate_password_hash,check_password_hash
 
 @login_manager.user_loader
 def load_user(user_id)
+    return user.query.get(init(user_id))
+
+
+
+class User(UserMixin,db.model)
+
+    __tablename__= 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.column(db.String(unique = True,index = True))
+    pass_secure = db.Column(db.String(255))
+    pitch = db.relationship('Pitch', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref= 'user', lazy='dynamic')
+    upvotes = db.relationship('upvote', backref = 'user', lazy = 'dynamic')
+    downvotes = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
+
+
+    @property
+    def pasword
