e6b0df2019e1d12e8b5963c90572ffabfdc550af
Patrick595 <patrickbuong@gmail.com>
2020-02-11 14:22:16 +0300

    created class models.py

--- app/__init__.py
+++ app/__init__.py
@@ -20,4 +20,34 @@ def create_app(config_name)
     '''
     Function that takes configeration setting key as an argument
 
-    Args
+    Args:
+        config_name : name of the configeration to be used
+    '''
+
+    # Initializing application
+    app = Flask(__name__)
+
+simple.init__app(name)
+
+# creating the appconfigeration
+app.config.from_object(config_options[config_name])
+
+
+#Initializing flask extensions
+bootstrap.init__app()
+db.init_app(app)
+login_Manager.init__app(app)
+
+# Registering the main Blueprint
+from.main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+
+# registering the auth blueprint
+from.auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+# setting config when using API
+#from.request import configure_request
+#config_request(app)
+
+    return app
