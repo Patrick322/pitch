570cc01bd44e029346665e265a919a91b28f039d
Patrick595 <patrickbuong@gmail.com>
2020-02-17 15:02:42 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -12,12 +12,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
   
 class TestConfig(Config):
     '''
